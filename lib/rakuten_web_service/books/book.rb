358d4ad39d2be571615af5e522b2d1171d1fbe5c
keyasuda <keyasuda@users.noreply.github.com>
2026-02-15 12:53:23 +0900

    update API endpoint cf. https://rakuten-webservice.tumblr.com/post/808140091326365696/importantnotice-2026-02-10-guide-regarding

--- lib/rakuten_web_service/books/book.rb
+++ lib/rakuten_web_service/books/book.rb
@@ -5,7 +5,7 @@ require 'rakuten_web_service/books/resource'
 module RakutenWebService
   module Books
     class Book < Books::Resource
-      endpoint 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404'
+      endpoint 'https://openapi.rakuten.co.jp/services/api/BooksBook/Search/20170404'
 
       attribute :title, :titleKana, :subTitle, :subTitleKana,
                 :seriesName, :seriesNameKana,
